05dcfc3103d28bbe503334aedf85b8ef2b83a34f
Vooradi <swetha.vooradi@capgemini.com>
2022-05-11 18:57:58 +0530

    fixed issue for dropdown

--- rm_app/src/views/Content.vue
+++ rm_app/src/views/Content.vue
@@ -4,7 +4,8 @@
       <v-select
         :items="$t('items')"
         item-text="name"
-        label="Select a Service"
+        :label="$t('placeholder')"
+        :v-model="$t(`items[${this.selectedLoan.name}]`)"
         solo
         return-object
         full-width
@@ -12,26 +13,23 @@
       ></v-select>
     </v-row>
     <v-card v-if="status" class="mx-auto" max-width>
-      <v-list-item three-line>
-        <v-list-item-content>
-          <div class="text-h5 mb-4">{{ $t("card_description") }}</div>
-          <v-list-item-subtitle>
-            <p>{{ $t(`items[${this.selectedLoan}].description`) }}</p>
-          </v-list-item-subtitle>
-          <v-radio-group v-model="radioGroup">
-            <v-radio
-              v-for="durations in $t('duration')"
-              :key="durations"
-              :label="durations"
-              :value="durations"
-            ></v-radio>
-          </v-radio-group>
-        </v-list-item-content>
-      </v-list-item>
-
-      <v-card-actions>
-        <v-btn outlined rounded text> {{ $t("button") }} </v-btn>
-      </v-card-actions>
+      <div class="ml-4">
+        <h1 class="mb-4">{{ $t("card_description") }}</h1>
+        <p >
+          {{ $t(`items[${this.selectedLoan.value}].description`) }}
+        </p>
+        <v-radio-group v-model="radioGroup">
+          <v-radio
+            v-for="durations in $t('duration')"
+            :key="durations"
+            :label="durations"
+            :value="durations"
+          ></v-radio>
+        </v-radio-group>
+        <v-card-actions>
+          <v-btn outlined rounded text> {{ $t("button") }} </v-btn>
+        </v-card-actions>
+      </div>
     </v-card>
   </div>
 </template>
@@ -40,12 +38,12 @@ export default {
   data: () => ({
     radioGroup: 1,
     status: false,
-    selectedLoan: '',
+    selectedLoan: "",
   }),
   methods: {
     onChangePage(a) {
-      this.status = true
-      this.selectedLoan = a.value
+      this.status = true;
+      this.selectedLoan = a;
     },
   },
 };
